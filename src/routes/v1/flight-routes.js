1cdd572a885a8f184fe6105080f7e88c36dda7b0
Saurav Sirswal <sauravsirswal11@gmail.com>
2026-02-03 15:06:18 +0530

    added get flight api

--- src/routes/v1/flight-routes.js
+++ src/routes/v1/flight-routes.js
@@ -11,4 +11,7 @@ router.post("/", FlightMiddlewares.validateCreateRequest, FlightController.creat
 // /api/v1/flights?trips=MUM-DEL GET
 router.get("/", FlightController.getAllFlights)
 
+// /api/v1/flights/:id GET
+router.get("/:id", FlightController.getFlight)
+
 module.exports = router
